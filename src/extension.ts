3aef12651ccf341d8f37dcd0822b258da68d9aec
Daking <dakingx@163.com>
2025-04-26 23:51:01 +0800

    support message history

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode'
 
 const BASE_PROMPT =
-	'You are a helpful code tutor. Your job is to teach the user with simple descriptions and sample code of the concept. Respond with a guided overview of the concept in a series of messages. Do not give the user the answer directly, but guide them to find the answer themselves. If the user asks a non-programming question, politely decline to respond.'
+	'You are a helpful code tutor. Your job is to teach the user with simple descriptions and sample code of the concept. Respond with a guided overview of the concept in a series of messages. Do not give the user the answer directly, but guide them to find the answer themselves. If the user asks a non-programming question, try to answer using prompt context or politely decline to respond.'
 
 export function activate(context: vscode.ExtensionContext) {
 	const handler: vscode.ChatRequestHandler = async (
@@ -10,10 +10,27 @@ export function activate(context: vscode.ExtensionContext) {
 		stream: vscode.ChatResponseStream,
 		token: vscode.CancellationToken,
 	) => {
-		const messages = [
-			vscode.LanguageModelChatMessage.User(BASE_PROMPT),
-			vscode.LanguageModelChatMessage.User(request.prompt),
-		]
+		const rolePrompt = BASE_PROMPT
+		console.log('Role Prompt:', rolePrompt)
+
+		const messages = [vscode.LanguageModelChatMessage.User(rolePrompt)]
+
+		// Message history
+		console.log('Context History:', context.history)
+		context.history.filter((chatTurn) => {
+			if (chatTurn instanceof vscode.ChatRequestTurn) {
+			console.log('Request Turn Prompt:', chatTurn.prompt)
+			messages.push(vscode.LanguageModelChatMessage.User(chatTurn.prompt))
+			} else if (chatTurn instanceof vscode.ChatResponseTurn) {
+				const fullMessage = chatTurn.response.filter(chatResponse => chatResponse instanceof vscode.ChatResponseMarkdownPart)
+					.map((markdownPart) => markdownPart.value.value)
+					.join('')
+				console.log('Response Turn Full Message:', fullMessage)
+				messages.push(vscode.LanguageModelChatMessage.Assistant(fullMessage))
+			}
+		})
+
+		messages.push(vscode.LanguageModelChatMessage.User(request.prompt))
 
 		const response = await request.model.sendRequest(messages, {}, token)
 
